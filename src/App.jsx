33b9d91e94e38daeb4dabe486e19ed2c461a4943
tsztin0217 <tsztin0217@gmail.com>
2025-03-27 20:47:35 -0400

    Render bookshelf in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 
-import './App.css'
-
+import './App.css';
+import Bookshelf from './components/Bookshelf/Bookshelf';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
-
-
